2362afa8080bf4058e1cdc547594a0169e00f33b
ShemeerCoder-01 <118423467+ShemeerCoder-01@users.noreply.github.com>
2023-04-13 14:34:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function insert_Row() {
     //Write your code here
   let row = document.getElementById("sampleTable");
-	row.innerHTML = `<tr><td>New Cell1</td><td>New Cell2</td></tr>`+ document.getElementById("sampleTable").innerHTML;
+	row.innerHTML = `<tr><td>New Cell 1</td><td>New Cell 2</td></tr>`+ document.getElementById("sampleTable").innerHTML;
 }
